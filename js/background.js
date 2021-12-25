5f9ced0e296df49ee12c9b31774b88ffb6ed13f6
kangsan lee <65530351+krlee92@users.noreply.github.com>
2021-12-25 21:01:08 +0900

    add weather.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,14 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
+
+
+
+
+/*const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+document.body.appendChild(bgImage);*/
